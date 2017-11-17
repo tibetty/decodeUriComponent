b4b60b7e61f72afdbc577fed372ded8bc9fa7e9a
git <xihua.duan@tourank.com>
2017-11-17 22:10:43 +0800

    add utf-8 support

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = s => s.replace(/%([0-9a-f]{2})/gi, (a, c) => String.fromCharCode(parseInt(c, 16))).replace(/%u([0-9a-f]{4})/gi, (a, u) => String.fromCodePoint(parseInt(u, 16)));
+module.exports = s => s.split(/(%u[0-9a-f]{4})/gi).map(v => {
+	if (v.match(/%u[0-9a-f]{4}/gi)) return String.fromCodePoint(parseInt(v.substring(2), 16));
+	else return decodeURIComponent(v);
+}).join('');
--- test.js
+++ test.js
@@ -2,15 +2,10 @@ const decodeUriComponent = require('./index.js');
 
 let ts2d = [
 	'%25',	//=> '%' 
-	'%',	//=> '%' 
 	'st%C3%A5le',	//=> 'ståle' 
-	'%st%C3%A5le%',	//=> '%ståle%'
-	'%%7Bst%C3%A5le%7D%',	//=> '%{ståle}%' 
-	'%7B%ab%%7C%de%%7D',	//=> '{%ab%|%de%}' 
-	'%FE%FF',	//=> '\uFFFD\uFFFD' 
-	'%C2',	//=> '\uFFFD' 
 	'%C2%B5',	//=> 'µ'
-	'2%2e8i%u624b%u52a85%u901f'	//=>'2.8i手动5速'
+	'2%2e8i%u624b%u52a85%u901f',	//=>'2.8i手动5速'
+	'%e6%9c%8d%e8%a3%85'
 ];
 
 ts2d.forEach((s) => console.log(decodeUriComponent(s)));
