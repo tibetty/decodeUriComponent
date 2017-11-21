42a8d34247b9408f76ac93293c84c33395a26730
git <xihua.duan@tourank.com>
2017-11-21 13:22:10 +0800

    more lambda-styled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const iconv = require('iconv-lite');
 
-module.exports = (s, enc) => 
-    enc? ((s, enc) => {
+module.exports = (s, enc) =>
+    enc ? ((s, enc) => {
         let regex = /(%([0-9a-f]{2}))+/gi;
         let res = null, lastIdx = 0, parts = [];
         while (res = regex.exec(s)) {
@@ -13,5 +13,5 @@ module.exports = (s, enc) =>
             lastIdx = regex.lastIndex;
         }
         return parts.join('');
-    })(s, enc) : 
-    (s => s.split(/(%u[0-9a-f]{4})/gi).map(v => v.match(/%u[0-9a-f]{4}/gi)? String.fromCodePoint(parseInt(v.substring(2), 16)) : decodeURIComponent(v)).join(''))(s);
+    })(s, enc) : (s => s.split(/(%u[0-9a-f]{4})/gi).map(v =>
+        v.match(/%u[0-9a-f]{4}/gi) ? String.fromCodePoint(parseInt(v.substring(2), 16)) : decodeURIComponent(v)).join(''))(s);
--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 const decodeUriComponent = require('./index.js');
 
-let ts2d = [
-	'%25',	//=> '%' 
-	'st%C3%A5le',	//=> 'ståle' 
-	'%C2%B5',	//=> 'µ'
-	'2%2e8i%u624b%u52a85%u901f'	//=>'2.8i手动5速'
-];
-
-ts2d.forEach((s) => console.log(decodeUriComponent(s)));
+console.log(decodeUriComponent('2%2e8i%u624b%u52a85%u901f'));
+console.log(decodeUriComponent('%e4%b8%ad%e5%9c%8b'));
 console.log(decodeUriComponent('%b7%fe%d7%b0', 'gbk'));
